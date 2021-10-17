acf30fda4560007b9ff4946897c6efb1429bfc1f
Bonnie Reed <bonniejeanreed@gmail.com>
2021-10-16 19:42:35 -0500

    fixed syntax issues in manager test file

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -6,17 +6,17 @@ const Manager = require('../lib/Manager');
 
 // creates the manager object
 test('creates an Manager object', () => {
-    const manager = new Manager('Amy', 22, 'amy@gmail.com', 2;
-    
+    const manager = new Manager('Amy', 22, 'amy@gmail.com', 2);
+
     expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
 // getRole() employee role is retrieved
 test('gets role of employee', () => {
-    const manager = new Manager('Amy', 22, 'amy@gmail.com', 2;
+    const manager = new Manager('Amy', 22, 'amy@gmail.com', 2);
 
-    expect(manager.getRole()).toEqual("Manager");
-}); 
+    expect(manager.getRole()).toEqual('Manager');
+});
 
 module.exports = {
     Manager,
